6c50b3bbc49ad73fde97852e99af3e9dfc85d7b9
Mike Busuttil <mikebusuttil@gmail.com>
2024-10-24 01:02:36 -0400

    version bump, migrate most of setup.py to pyproject.toml

--- setup.py
+++ setup.py
@@ -1,69 +1,24 @@
 ﻿from pathlib import Path
 from setuptools import setup, find_packages
 from yaml import safe_load
+import toml
 
 if __name__ == "__main__":
+    pyproject = toml.load(Path(__file__).parent / 'pyproject.toml')
     setup(
-        name = 'sigfig',
-        description = 'Python library for rounding numbers (with expected results)',
+        name = pyproject['tool']['poetry']['name'],
+        description = pyproject['tool']['poetry']['description'],
 
         version = safe_load(open(Path(__file__).parent / 'CITATION.cff'))['version'],
         license = 'MIT',
-        url = 'https://sigfig.readthedocs.io/',
-        docs_url = 'https://sigfig.readthedocs.io/',
-        bugtrack_url = 'https://github.com/drakegroup/sigfig/issues',
-        install_requires =['SortedContainers'],
+        url = pyproject['tool']['poetry']['urls']['Homepage'],
+        install_requires = ['SortedContainers'],
         packages = find_packages(exclude = ['doc', 'test']),
         long_description = open(Path(__file__).parent / 'README.rst', encoding='utf-8').read(),
         long_description_content_type = 'text/x-rst',
-        author = 'Mike & Travis',
-        author_email = 'mike.busuttil@gmail.com, valdezt@gmail.com',
-        maintainer = 'Mike Busuttil',
-        maintainer_email = 'mike.busuttil@gmail.com',
+        author = pyproject['tool']['poetry']['authors'],
+        maintainer = pyproject['tool']['poetry']['maintainers'],
 
-        keywords = [
-            'round', 'rounding', 'significant figures', 'significant digits',
-            'sigfigs', 'sigdigs', 'decimals', 'uncertainty', 'uncertainties',
-            'numeric', 'numerical', 'number', 'numbers', 'data',
-            'format', 'style',  'publication'
-        ],
-        classifiers = [
-            'Development Status :: 5 - Production/Stable',
-            'Intended Audience :: Developers',
-            'Intended Audience :: Education',
-            'Intended Audience :: Financial and Insurance Industry',
-            'Intended Audience :: Healthcare Industry',
-            'Intended Audience :: Other Audience',
-            'Intended Audience :: Manufacturing',
-            'Intended Audience :: Science/Research',
-            'License :: OSI Approved :: MIT License',
-            'Operating System :: OS Independent',
-            'Programming Language :: Python :: 2',
-            'Programming Language :: Python :: 3',
-            'Programming Language :: Python :: 2.7',
-            'Programming Language :: Python :: 3.0',
-            'Programming Language :: Python :: 3.1',
-            'Programming Language :: Python :: 3.2',
-            'Programming Language :: Python :: 3.3',
-            'Programming Language :: Python :: 3.4',
-            'Programming Language :: Python :: 3.5',
-            'Programming Language :: Python :: 3.6',
-            'Programming Language :: Python :: 3.7',
-            'Programming Language :: Python :: 3.8',
-            'Programming Language :: Python :: 3.9',
-            'Programming Language :: Python :: 3.10',
-            'Programming Language :: Python :: 3.11',
-            'Programming Language :: Python :: 3.12',
-            'Topic :: Education',
-            'Topic :: Education :: Computer Aided Instruction (CAI)',
-            'Topic :: Office/Business :: Financial',
-            'Topic :: Scientific/Engineering',
-            'Topic :: Scientific/Engineering :: Information Analysis',
-            'Topic :: Scientific/Engineering :: Mathematics',
-            'Topic :: Scientific/Engineering :: Physics',
-            'Topic :: Software Development',
-            'Topic :: Software Development :: Libraries',
-            'Topic :: Software Development :: Libraries :: Python Modules',
-            'Topic :: Utilities',
-        ]
+        keywords = pyproject['tool']['poetry']['keywords'],
+        classifiers = pyproject['tool']['poetry']['classifiers'],
     )
